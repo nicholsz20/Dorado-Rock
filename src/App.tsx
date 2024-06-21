da2b4c966f704b40b9bcdc9599841cdfcd24b401
Nicholsz20 <zachnichols1313@gmail.com>
2024-06-21 15:41:04 -0400

    Updated htaccess file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./pages/AboutUs";
 import ContactForm from "./pages/ContactForm";
@@ -13,9 +13,10 @@ function App() {
       <Routes>
         <Route path={"/"} element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/spirits" element={<ProductPage />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactForm />} />
+          <Route path="spirits" element={<ProductPage />} />
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="contact-us" element={<ContactForm />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </BrowserRouter>
